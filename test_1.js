c69dcd923ac9828bba66ebb38a28fcbc2fe8d934
TooDiMan <yarik9905@gmail.com>
2021-07-27 18:04:18 +0300

    123

--- test_1.js
+++ test_1.js
@@ -1,5 +1,8 @@
-<script type="text/javascript">
-…
-Код javascript
-…
-</script>
+function displayTime() {
+	var element = document.getElementById("clock"); // Найти элемент с id="clock" 
+	var now = new Date();	// Получить текущее время
+	element.innerHTML = now.toLocaleTimeString(); // Отобразить его 
+	setTimeout(displayTime, 1000);	// Вызвать снова через 1 сек
+}
+
+window.onload = displayTime; // Начать отображение времени после загрузки документа
